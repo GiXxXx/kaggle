5cb3858c213bc14a183575467267b0cb9c722147
Jason <jason.gao@sgdigital.com>
2019-07-13 15:09:20 +0800

    xgb simple stacking

--- digital-recognizer/simple-stacking.py
+++ digital-recognizer/simple-stacking.py
@@ -11,15 +11,19 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import BaggingClassifier
 from mlxtend.classifier import StackingClassifier
 
-train_data = pd.read_csv("~/kaggle-workspace/kaggle/digital-recognizer/train.csv")
-test_data = pd.read_csv("~/kaggle-workspace/kaggle/digital-recognizer/test.csv")
+from xgboost import XGBClassifier
+
+train_data = pd.read_csv(
+    "~/kaggle-workspace/kaggle/digital-recognizer/train.csv")
+test_data = pd.read_csv(
+    "~/kaggle-workspace/kaggle/digital-recognizer/test.csv")
 
 # pca=PCA(n_components=100, random_state=34)
 # train_data_pca=pca.fit_transform(train_data.values[:,1:])
 # test_data_pca=pca.fit_transform(train_data.values)
 
 train_data_pca = train_data.values[:, 1:]
-test_data_pca=test_data.values
+test_data_pca = test_data.values
 
 # lm_clf = SVC()
 # lm_clf.fit(train_data_pca,train_data.values[:, 0])
@@ -37,27 +41,42 @@ knn_clf = KNeighborsClassifier(n_jobs=-1)
 # svc_clf = SVC(C=4.0)
 
 rf_clf = RandomForestClassifier(n_estimators=110, max_depth=5,
-                                                  min_samples_split=2, min_samples_leaf=1, random_state=34, n_jobs=-1)
+                                min_samples_split=2, min_samples_leaf=1, random_state=34, n_jobs=-1)
 
-st_clf = StackingClassifier(classifiers=[knn_clf, rf_clf], meta_classifier=LogisticRegression(), use_probas=True, average_probas=False)
+xgb_clf = XGBClassifier(max_depth=5, learning_rate=0.3, n_estimators=100,n_jobs=-1,random_state=34)
+
+st_clf = StackingClassifier(classifiers=[knn_clf, rf_clf, xgb_clf], meta_classifier=LogisticRegression(
+), use_probas=True, average_probas=False)
 
 
-print('3-fold cross validation:\n')
+# print('3-fold cross validation:\n')
 
-for clf, label in zip([knn_clf, rf_clf, st_clf],
-                      [
-                       'KNN',
-                       'Random Forest',
-                       'StackingClassifier']):
+# for clf, label in zip([knn_clf, rf_clf, st_clf],
+#                       [
+#     'KNN',
+#     'Random Forest',
+#         'StackingClassifier']):
 
-    scores = model_selection.cross_val_score(
-        clf, train_data_pca, train_data.values[:, 0], cv=3, scoring='accuracy')
-    print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+#     scores = model_selection.cross_val_score(
+#         clf, train_data_pca, train_data.values[:, 0], cv=3, scoring='accuracy')
+#     print("Accuracy: %0.2f (+/- %0.2f) [%s]" %
+#           (scores.mean(), scores.std(), label))
 
-# ret = bagging_clf.predict(test_data.values)
+# for clf, label in zip([xgb_clf],
+#                       [
+#         'XGB']):
 
-# result = pd.DataFrame(data=test_data.index, index=None, columns=['ImageId'])
-# result['Label'] = ret
-# result.ImageId = result.ImageId + 1
+#     scores = model_selection.cross_val_score(
+#         clf, train_data_pca, train_data.values[:, 0], cv=2, scoring='accuracy')
+#     print("Accuracy: %0.2f (+/- %0.2f) [%s]" %
+#           (scores.mean(), scores.std(), label))
 
-# result.to_csv('simple-stacking.csv', index=False)
+st_clf.fit(train_data_pca, train_data.values[:, 0])
+
+ret = st_clf.predict(test_data.values)
+
+result = pd.DataFrame(data=test_data.index, index=None, columns=['ImageId'])
+result['Label'] = ret
+result.ImageId = result.ImageId + 1
+
+result.to_csv('simple-stacking.csv', index=False)
